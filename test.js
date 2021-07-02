2da0dad9aabc886f966fb03bb79ae23911dfacd9
chen.qianqian <chen.qianqian@navercorp.com>
2021-07-02 17:20:49 +0800

    update files

--- test.js
+++ test.js
@@ -12,46 +12,46 @@ async function gitCommit() {
     if (e.code === 128) {
       await spawn([".git/index.lock"], "rm");
     }
-    await gitExcute(msg);
+    await gitExcute();
   }
 }
 async function gitExcute(msg) {
   await excute(["add", "."]);
-  console.log("commit");
   await excute(["commit", "-m", msg]);
-  await excute(["push", "--set-upstream", "origin", Git.branch()]);
+  // await excute(["push", "--set-upstream", "origin", Git.branch()]);
 }
 
 async function excute(params, command = "git") {
-  const task = spawn(command, params, {
-    cwd: process.cwd(),
-    stdio: "inherit",
-  });
-  console.log(task);
-  if (task.stdout) {
-    task.stdout.on("data", (data) => {
-      console.log(`stdout: ${data}`);
+  return new Promise((resolve) => {
+    const task = spawn(command, params, {
+      cwd: process.cwd(),
+      stdio: "inherit",
     });
-  }
-
-  if (task.stderr) {
-    task.stderr.on("data", (data) => {
-      console.error(`stderr: ${data}`);
-    });
-  }
-  task.on("close", (code) => {
-    console.log("close");
-    if (code) {
-      const e = new Error("command execute failed");
-      e.code = code;
-      throw new Error(e);
+    if (task.stdout) {
+      task.stdout.on("data", (data) => {
+        console.log(`stdout: ${data}`);
+      });
     }
-    process.exit(0);
-  });
-  task.on("exit", (e) => {
-    console.log(params);
-    process.exit(e);
-    return Promise.resolve(e);
+
+    if (task.stderr) {
+      task.stderr.on("data", (data) => {
+        console.error(`stderr: ${data}`);
+      });
+    }
+    task.on("close", (code) => {
+      console.log("close");
+      if (code) {
+        const e = new Error("command execute failed");
+        e.code = code;
+        throw new Error(e);
+      }
+      // process.exit(0);
+    });
+    task.on("exit", (e) => {
+      console.log(params);
+      // process.exit(e);
+      resolve(e);
+    });
   });
 }
 async function gitCommitMsg() {
